0e246a4c54699ef6de0d56d1b6b360ce52a80da1
c0647015 <c0647015@SB-N208-03.lambton.on.ca>
2015-01-14 15:41:16 -0500

    created method for new order arrives

--- Assignment1/src/assignment1/Assignment1.java
+++ Assignment1/src/assignment1/Assignment1.java
@@ -18,5 +18,7 @@ public class Assignment1 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public void newOrderArrives(Order order){
+        order.getCustomerId();
+    }
 }
